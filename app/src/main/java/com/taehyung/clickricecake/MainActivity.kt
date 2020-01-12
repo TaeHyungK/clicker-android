1c534b20147db12b3ad232727d4c3ed56ad6b22f
taehyungK <xogud3125@naver.com>
2020-01-12 17:36:51 +0900

    [메인] 떡국 추가 clickListener 람다식으로 변경

--- app/src/main/java/com/taehyung/clickricecake/MainActivity.kt
+++ app/src/main/java/com/taehyung/clickricecake/MainActivity.kt
@@ -3,7 +3,6 @@ package com.taehyung.clickricecake
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,17 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        main_soup_img.setOnClickListener(mClickListener)
-        main_soup_making.text = "1초당 ${mAutoMaking}개 생산중 !"
-
-    }
-
-    private val mClickListener = object: View.OnClickListener {
-        override fun onClick(view: View?) {
-            if (view?.id == main_soup_count.id) {
-                mTotalMaking += mClickMaking
-                main_soup_count.text = mTotalMaking.toString()
-            }
+        main_soup_img.setOnClickListener {
+            mTotalMaking += mClickMaking
+            main_soup_count.text = mTotalMaking.toString()
         }
+        main_soup_making.text = "1초당 ${mAutoMaking}개 생산중 !"
     }
 }

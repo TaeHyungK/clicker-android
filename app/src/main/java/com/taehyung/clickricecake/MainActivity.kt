324010c86cd11d936f1b08b3fc69a728c776c8e5
taehyungK <xogud3125@naver.com>
2020-01-15 00:38:57 +0900

    [메인] 아이템 클릭 시 구매 팝업 노출 처리

--- app/src/main/java/com/taehyung/clickricecake/MainActivity.kt
+++ app/src/main/java/com/taehyung/clickricecake/MainActivity.kt
@@ -2,8 +2,10 @@ package com.taehyung.clickricecake
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_purchase.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,15 +30,34 @@ class MainActivity : AppCompatActivity() {
             main_soup_count.text = mTotalMaking.toString()
         }
         main_item_dog.setOnClickListener {
-            Toast.makeText(applicationContext, "댕댕이 클릭!", Toast.LENGTH_SHORT).show()
+            makeDialog("댕댕이")
         }
         main_item_baby.setOnClickListener {
-            Toast.makeText(applicationContext, "뽀시래기 클릭!", Toast.LENGTH_SHORT).show()
+            makeDialog("뽀시래기")
         }
         main_item_kids.setOnClickListener {
-            Toast.makeText(applicationContext, "급식동생 클릭!", Toast.LENGTH_SHORT).show()
+            makeDialog("급식동생")
         }
+    }
 
+    /**
+     * 구매/취소 Dialog 생성
+     */
+    fun makeDialog(item: String) {
+        val builder = AlertDialog.Builder(this)
+        val dialogView = layoutInflater.inflate(R.layout.dialog_purchase, null)
 
+        dialogView.dialog_title.text = "구매하기"
+        dialogView.dialog_desc.text = getString(R.string.text_purchase, item)
+
+        builder.setView(dialogView)
+                .setPositiveButton(getString(R.string.btn_purchase)) { dialogInterface, i ->
+                    // TODO 확인일 경우 RecyclerView에 아이템 추가 처리필요
+                    Toast.makeText(applicationContext, "$item 구매!", Toast.LENGTH_SHORT).show()
+                }
+                .setNegativeButton(R.string.btn_cancel) { dialogInterface, i ->
+                    // 취소일 경우 동작 없음
+                }
+                .show()
     }
 }

c39bd8fe5d8880385968dac8df86eabbe2bab474
taehyungK <xogud3125@naver.com>
2020-01-14 23:25:48 +0900

    [메인] 아이템 추가(임시)  - 댕댕이, 뽀시래기, 급식동생 추가

--- app/src/main/java/com/taehyung/clickricecake/MainActivity.kt
+++ app/src/main/java/com/taehyung/clickricecake/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.taehyung.clickricecake
 
-import androidx.appcompat.app.AppCompatActivity
-
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,10 +16,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // set Text value
+        main_soup_making.text = "1초당 ${mAutoMaking}개 생산중 !"
+        main_item_dog.text = "🐶/댕댕이/1/1dk/s"
+        main_item_baby.text = "👶/뽀시래기/2/2dk/s"
+        main_item_kids.text = "🙇‍️/급식동생/3/3dk/s"
+
+        // set clickListener
         main_soup_img.setOnClickListener {
             mTotalMaking += mClickMaking
             main_soup_count.text = mTotalMaking.toString()
         }
-        main_soup_making.text = "1초당 ${mAutoMaking}개 생산중 !"
+        main_item_dog.setOnClickListener {
+            Toast.makeText(applicationContext, "댕댕이 클릭!", Toast.LENGTH_SHORT).show()
+        }
+        main_item_baby.setOnClickListener {
+            Toast.makeText(applicationContext, "뽀시래기 클릭!", Toast.LENGTH_SHORT).show()
+        }
+        main_item_kids.setOnClickListener {
+            Toast.makeText(applicationContext, "급식동생 클릭!", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
